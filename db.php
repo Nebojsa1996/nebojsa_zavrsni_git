6e9a716c24fc9aa29ac87a6a0c8e20fece7bcac7
Nebojsa1996 <mrsmrs.tamo@yahoo.com>
2022-06-07 22:02:44 +0200

    db file changed

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+// $servername = "localhost";
+// $username = "academy";
+// $password = "academy";
+// $dbname = "blog";
+
+// // Create connection !!
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Check connection !!
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+
+// try {
+// $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// set the PDO error mode to exception
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
